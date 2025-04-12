8dcb23420d4a185641ce3a5e53558ba5c0871d3b
HAZEhithub <harsh01786@gmail.com>
2025-04-12 20:15:22 +0530

    fix: add dummy server to keep Render alive

--- bot.js
+++ bot.js
@@ -32,4 +32,14 @@ fs.readdirSync("./commands").forEach(file => {
 require("./utils/cronJobs")(bot);
 require("./utils/inlineButtons")(bot);
 
-require('http').createServer(() => {}).listen(3000);
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Service is running');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
